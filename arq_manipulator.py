2faf26cce7bcd834e421c0b72cce548bbd816f15
rubensalexsander <root@pc1.pc1.com>
2022-09-06 19:10:22 -0300

    commit-06-09-2022

--- arq_manipulator.py
+++ arq_manipulator.py
@@ -4,20 +4,33 @@ def list_to_arq(lt):
 class Arq_txt:
     def __init__(self, local_file):
         self.local_file = local_file
+        self.get_lines()
     
     def connect(self, open_form='r'):
         try:
             arq = open(self.local_file, open_form)
             return arq
-        except: return False
-    
+        except:
+            print(f"Erro ao conectar em {self.local_file}")
+            return None
+
     def desconnect(self, arq):
         arq.close()
+    
+    def find(self, content):
+        self.get_lines()
+        locais = []
+        for line in self.lines:
+            if content in line:
+                locais.append(self.lines.index(line))
+        return locais
 
-    def lines(self):
+    def get_lines(self, line="all"):
         arq = self.connect('r')
         self.lines = arq.readlines()
         self.desconnect(arq)
+        if line != "all":
+            return self.lines[line]
         return self.lines
     
     def rewrite_all(self, content=['']):
@@ -37,7 +50,29 @@ class Arq_txt:
         self.desconnect(arq)
 
 def main():
-    print('txt_manipulate')
+    while True:
+        local = input("\nLocal: ")
+        arqm = Arq_txt(local)
+
+        while arqm:
+            print(f"\n## Arq_manipulator: {local} ##\n\nc - Trocar arquivo\n0 - Ver arquivos\n1 - Alterar linha\n2 - Encontrar linha\n")
+            resp = input("> ")
+
+            if resp == "c":
+                break
+
+            if resp == "0":
+                print("\n"+"-"*30)
+                for line in arqm.get_lines(): print(line)
+                print("\n"+"-"*30+"\n")
+
+            elif resp == "1":
+                texto = input("\nTexto:\n> ")
+                linha = int(input("\nLinha:\n> "))
+                arqm.rewrite_line(content=texto, line=linha)
+            elif resp == "2":
+                texto = input("\nTexto:\n> ")
+                print(f"\nOcorrências de {texto} no arquivo: "+str(arqm.find(content=texto)))
 
 if __name__ == '__main__':
     main()
